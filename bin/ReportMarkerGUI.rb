6102fa7edcc7f3e7286408f006f5441cfe43ae6d
Mat Gilbert <freefallertam@gmail.com>
2012-05-17 13:23:20 +0100

    All checkboxes named

--- bin/ReportMarkerGUI.rb
+++ bin/ReportMarkerGUI.rb
@@ -3,19 +3,19 @@ class ReportMarkerGUI < ReportMarker
 	include GladeGUI
 
 	def initialize
-		@r1 = false
+		@r1 = true
 		@r2 = false
 	end
 
 	def show()
 		load_glade(__FILE__)  #loads file, glade/MyClass.glade into @builder
-		set_glade_all(self) #populates glade controls with insance variables (i.e. Myclass.var1) 
+		set_glade_all(self) #populates glade controls with insance variables (i.e. Myclass.var1)
 		show_window()
-	end	
+	end
 	
 	def buttonSave__clicked(button)
 		get_glade_all()
-		VR::Dialog.message_box("Reqirement 1) #{@r1} 2) #{@r2}")		
+		VR::Dialog.message_box("Reqirement 1) #{@r1} 2) #{@r2}")
 	end
 
 end
