0430fa8ad6f8c25851b859a78fb2e829537c5617
Mat Gilbert <freefallertam@gmail.com>
2012-05-17 15:46:17 +0100

    All instance checkboxes initialised

--- bin/ReportMarkerGUI.rb
+++ bin/ReportMarkerGUI.rb
@@ -3,8 +3,14 @@ class ReportMarkerGUI < ReportMarker
 	include GladeGUI
 
 	def initialize
-		@r1 = false
-		@r2 = false
+		@r1 = @r2 = false
+    @a1 = @a2 = @a3 = @a4 = @a5 =@a5_2 = @a6 = @a6_2 = false
+    @a7 = @a7_2 = false
+    @s1 = @s2 = @s3 = false
+    @d1 = @d2 = @d2_2 = @d3 = @d3_2 = @d4 = @d4_2 = @d5 = false
+    @d5_2 = @d5_3 = @d6 = @d6_2 = @d7 = @d7_2 = false
+    @d8 = @d8_2 = @d8_3 = @d9 = @d9_2 = @d9_3 = @d10 = false
+    @d10_2 = @d10_3 = @d11 = @d11_2 = false
 	end
 
 	def show()
